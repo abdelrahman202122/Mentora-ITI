d2b35782d7250b17ae15f31c261c74efd096ff92
Marina-Michel999 <marinamichel@gmail.com>
2026-06-14 01:06:06 +0200

    handle localization by next intl

---
+++ apps/web/src/app/[locale]/layout.tsx
@@ -0,0 +1,21 @@
+import { NextIntlClientProvider } from "next-intl";
+
+export default async function LocaleLayout({
+  children,
+  params
+}: {
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider locale={locale}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
