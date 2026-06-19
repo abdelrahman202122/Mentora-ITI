e8c877c683e508138963354198cdfadf3a121979
Ahmed Tarek <atoc.atoc.007@gmail.com>
2026-06-19 23:32:10 +0300

    Modified env file with paymob parameters and created payment congfig

---
+++ apps/api/src/config/paymob.config.ts
@@ -0,0 +1,20 @@
+import { env } from './env.js';
+
+export const paymobConfig = {
+    apiKey: env.PAYMOB_API_KEY,
+
+    secretKey:
+        env.PAYMOB_SECRET_KEY,
+
+    publicKey:
+        env.PAYMOB_PUBLIC_KEY,
+
+    hmacSecret:
+        env.PAYMOB_HMAC_SECRET,
+
+    integrationId:
+        env.PATMOB_INTEGRATION_ID,
+
+    baseUrl:
+        "https://accept.paymob.com"
+};
