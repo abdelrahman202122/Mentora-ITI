8f8e8bb4b8c487ebb313b1d6bff68d14223f7874
KhaledSaeed155 <khaledbeboo155@gmail.com>
2026-06-20 06:56:10 +0300

    Add home-page components

--- apps/web/src/app/Home/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { AuthGuard } from "@/components/auth/auth-guard";
-
-export default function HomeLayout({ children }: { children: React.ReactNode }) {
-  return <AuthGuard>{children}</AuthGuard>;
-}
--- apps/web/src/app/[locale]/page.tsx
+++ apps/web/src/app/[locale]/page.tsx
@@ -1,5 +1,20 @@
 import { Button } from '@/components/ui/button';
+import Header from './component/Header/Header';
+import Hero from './component/Hero/Hero';
+import SubjectSection from './component/SubjectsSection/SubjectsSection';
+import GradeLevels from './component/GradeLevels/GradeLevels';
+import HowItWorks from './component/HowItWorks/HowItWorks';
+import Reviews from './component/Reviews/Reviews';
 
 export default function Home() {
-  return <Button variant="outline">Welcome to Mentora</Button>;
+  return (
+    <>
+    <Header />
+    <Hero />
+    <SubjectSection />
+    <GradeLevels />
+    <HowItWorks />
+    <Reviews />
+    </>
+  );
 }
