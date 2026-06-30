26f8f0ef520b70845541053b91cecf9b1476cc94
verinak <verinamichelk@gmail.com>
2026-06-30 14:57:49 +0300

    feat(tutors): get stats for tutor dashboard

--- apps/api/src/modules/tutor/tutor.controller.ts
+++ apps/api/src/modules/tutor/tutor.controller.ts
@@ -1,10 +1,21 @@
 import type { Request, Response } from 'express';
-import { getTutors } from './tutor.service.js';
-import { sendSuccess } from '../../utils/api-response.js';
+import { getTutors, getTutorStats } from './tutor.service.js';
+import { sendError, sendSuccess } from '../../utils/api-response.js';
 import type { TutorSearchParams } from '../../validators/tutor-search.js';
 
 export const getTutorsController = async (req: Request, res: Response) => {
   const result = await getTutors(req.query as unknown as TutorSearchParams);
 
   return sendSuccess(res, 200, 'Tutors fetched successfully', result);
+};
+
+export const getTutorStatsController = async (req: Request, res: Response) => {
+  const userId = req.user?.userId;
+
+  if (!userId) {
+    return sendError(res, 401, 'Unauthorized');
+  }
+  const result = await getTutorStats(userId);
+
+  return sendSuccess(res, 200, 'Tutors stats fetched successfully', result);
 };
